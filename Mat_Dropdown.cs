4d438483ea82471d440fcae196d07720467bd812
John <47099626+Gameartist3d@users.noreply.github.com>
2019-01-28 18:47:56 -0500

    Add files via upload

---
+++ Mat_Dropdown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Mat_Dropdown : MonoBehaviour {
+
+    public Dropdown mdropdown;
+    public GameObject Mycar;
+    public Color mcolor;
+    MeshRenderer mRender;
+    List<string> names = new List<string>() { "Red", "Blue", "Green", "Yellow" };
+    int listindex;
+
+    void Start () {
+        PopulateList();
+	}
+
+    private void Update()
+    {
+        mRender = GetComponent<MeshRenderer>();
+        mRender.material.color = mcolor;
+    }
+
+    void PopulateList()
+    {
+        mdropdown.AddOptions(names);
+    }
+	
+}
