3ca02d03a14542a40dbf90c53bc17ee36ac36f96
Gameartist3d <47099626+Gameartist3d@users.noreply.github.com>
2019-01-28 02:40:36 -0500

    Create RotateObj.cs

---
+++ RotateObj.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObj : MonoBehaviour {
+
+    float rotSpeed = 10;
+
+    private void OnMouseDrag()
+    {
+        float rotx = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround (Vector3.up, rotx);
+    }
+}
