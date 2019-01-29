88b43ee0e6502f65c3a25c174e67c798d7d4eac0
John <47099626+Gameartist3d@users.noreply.github.com>
2019-01-28 19:11:38 -0500

    Update Mat_Dropdown.cs

--- Mat_Dropdown.cs
+++ Mat_Dropdown.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 public class Mat_Dropdown : MonoBehaviour {
 
     public Dropdown mdropdown;
-    public GameObject Mycar;
+    public GameObject Mycar;//designates the model who's material is being changed
     public Color mcolor;
     MeshRenderer mRender;
-    List<string> names = new List<string>() { "Red", "Blue", "Green", "Yellow" };
+    List<string> names = new List<string>() { "Red", "Blue", "Green", "Yellow" };//list of options for dropdown menu
     int listindex;
 
     void Start () {
@@ -19,6 +19,7 @@ public class Mat_Dropdown : MonoBehaviour {
     {
         mRender = GetComponent<MeshRenderer>();
         mRender.material.color = mcolor;
+	    //TODO Use index values of names list to change material assigned
     }
 
     void PopulateList()
